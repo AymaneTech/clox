732c786a509df03a6dda880d2daf854834ef9bca
Aymane El Maini <elmainiaymane03@gmail.com>
2025-12-08 22:21:38 +0100

    feat: add all binary operations

--- src/vm.c
+++ src/vm.c
@@ -39,6 +39,12 @@ static InterpretResult run()
 {
 #define READ_BYTE()	(*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op)                                                          \
+	do {                                                                   \
+		double b = pop();                                              \
+		double a = pop();                                              \
+		push(a op b);                                                  \
+	} while (false)
 
 	for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -58,6 +64,18 @@ static InterpretResult run()
 			push(constant);
 			break;
 		}
+		case OP_ADD:
+			BINARY_OP(+);
+			break;
+		case OP_SUBSTRACT:
+			BINARY_OP(-);
+			break;
+		case OP_MULTIPLY:
+			BINARY_OP(*);
+			break;
+		case OP_DIVIDE:
+			BINARY_OP(/);
+			break;
 		case OP_NEGATE:
 			push(-pop());
 			break;
@@ -71,6 +89,7 @@ static InterpretResult run()
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk)
